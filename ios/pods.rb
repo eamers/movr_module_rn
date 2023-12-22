2e7496e17d24dd8d17d34220a7f7374b50090142
Brendan Eamer <brendan.eamer@gmail.com>
2023-12-21 23:14:44 -0800

    Updates SDK

--- ios/pods.rb
+++ ios/pods.rb
@@ -5,7 +5,7 @@ def use_movr_module_rn! (options={})
   packageName = package['name']
   prefix = options[:path] ||= "../node_modules/#{packageName}"
 
-  pod 'Flutter', :podspec => "#{prefix}/modules/ios/framework/Debug/Flutter.podspec"
+  pod 'Flutter', :podspec => "#{prefix}/modules/ios/framework/Release/Flutter.podspec"
   pod 'FlutterModuleFrameworks-Debug',
     :configuration => 'Debug',
     :podspec => "#{prefix}/ios/Podspecs/MovrModuleRn-Debug.podspec"
